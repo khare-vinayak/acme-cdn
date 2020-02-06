2dc563b74e8c71dccbb2077ff7497975c59c07d6
Vinayak Khare <khare.vinayak@gmail.com>
2020-02-05 22:54:26 -0500

    Completed

--- app.js
+++ app.js
@@ -1,32 +1,109 @@
-const products = document.querySelector('#products');
-const companies = document.querySelector('.companies');
+const resultsTable = document.querySelector('#results');
 
-const productsPromise = axios.get('https://acme-users-api-rev.herokuapp.com/api/products')
-const companiesPromise = axios.get('https://acme-users-api-rev.herokuapp.com/api/companies')
+const nav=document.querySelector('.nav-tabs');
+//console.log(nav);
 
-Promise.all([productsPromise, companiesPromise])
+
+const productsPromise   = axios.get('https://acme-users-api-rev.herokuapp.com/api/products')
+const companiesPromise  = axios.get('https://acme-users-api-rev.herokuapp.com/api/companies')
+
+const dataPromise = Promise.all([productsPromise, companiesPromise])
     .then(responses => {
-        renderProducts(responses[0].data);
-        renderCompanies(responses[1].data);
-    })
+        return responses.map(response=>response.data);
+        });
 
+dataPromise.then (results => {
+    renderNavigation(results);
+});
+    
+const renderNavigation = (results=>{
+    
+    const [products,companies]=results;
+    const tabs=`
+        <li class="nav-item">
+            <a class="nav-link active" href="#products" id="product">Products (${products.length})
+            </a> 
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#companies" id="company">Companies (${companies.length})
+            </a>
+        </li>`
+    nav.innerHTML=tabs;
+    renderProducts(products);
+    //renderCompanies(companies);
+});       
+    
+    
 
+window.addEventListener('hashchange',()=>{
+    id=window.location.hash.slice(1);
+    console.log(id);
+    console.log(nav);
+   
+    dataPromise.then(results=>{
+        const [products,companies]=results;
+        
+    //console.log(document.querySelectorAll('.nav-link').);
+    const navLinks=[...document.querySelectorAll('.nav-link')];
+    console.log(navLinks);
+   
+     if(id==='products'){
+        navLinks[0].classList.add('active');
+        navLinks[1].classList.remove('active');
+        renderProducts(products);
+    }
+    else if(id==='companies'){
+        navLinks[0].classList.remove('active');
+        navLinks[1].classList.add('active');
+        renderCompanies(companies);
+    } 
+    });    
+});
 
 const renderProducts = (products) => {
-    let html = products.map(product => {
+    
+    const productsHeading=Object.keys(products[0]);
+    const htmlHeading=productsHeading.map(cols=>{
+        return `        
+                <th>${cols.toUpperCase()}</th>
+                `
+    }).join('');
+
+    const html = products.map(product => {
         return `
-        <th>${product.id}</th>
-        <th>${product.name}</th>
-        <th>${product.description}</th>
-        <th>${product.suggestedPrice}</th>
-        <th>${product.createdAt}</th>
-        <th>${product.updatedAt}</th>
+        <tr>
+            <td>${product.id}</td>
+            <td>${product.name}</td>
+            <td>${product.description}</td>
+            <td>${product.suggestedPrice}</td>
+            <td>${product.createdAt}</td>
+            <td>${product.updatedAt}</td>
+        </tr>
         `
     }).join('')
-    products.innerHTML = html;
-    console.log(html);
+    resultsTable.innerHTML = htmlHeading.concat(html);
+    
 }
+const renderCompanies = (companies) => {
+    const companiesHeading=Object.keys(companies[0]);
+    const htmlHeading=companiesHeading.map(cols=>{
+        return `        
+                <th>${cols.toUpperCase()}</th>
+                `
+    }).join('');
 
-const renderCompanies = (company) => {
-
+    const html = companies.map(company => {
+        return `
+        <tr>
+            <td>${company.id}</td>
+            <td>${company.name}</td>
+            <td>${company.phone}</td>
+            <td>${company.state}</td>
+            <td>${company.catchPhrase}</td>
+            <td>${company.createdAt}</td>
+            <td>${company.updatedAt}</td>
+        </tr>
+        `
+    }).join('')
+    resultsTable.innerHTML = htmlHeading.concat(html);
 }
